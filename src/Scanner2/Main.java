397bb963234e65c635c3f1b6fd072cfe56257c4d
zepolcore <martinlopezutn@gmail.com>
2023-12-27 23:58:11 -0300

    Scanner 2

---
+++ src/Scanner2/Main.java
@@ -0,0 +1,9 @@
+package Scanner2;
+//Realiza un progarma que calcule la nota que hace falta sacar en el segundo examen de la asignatura programacion
+//para obtener la media deseada. Hay que tener en cuenta que la nota del primer examen cuenta el 40% y la del 2do un 60%
+public class Main {
+    public static void main(String[] args){
+
+    }
+
+}
